5c8880b6fdd71527c0886a7aa4530bf89fdba549
pseguel27 <pato.seguel.moya@gmail.com>
2017-12-06 18:52:50 -0300

    trabajando con service, parametros en el router, *ngFor y *ngIf

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+	heroes:Heroe[] = [];
 
-  ngOnInit() {
-  }
+	constructor( private _heroesService:HeroesService, private router: Router) {
+		
+		//console.log("constructor");
+	
+	}
+
+	ngOnInit() {
+
+		//trae los heroes del servicio
+		this.heroes = this._heroesService.getHeroes();
+
+	}
+
+	verHeroe( idx:number ){
+
+		//se llama al pinchar un heroe y le pasa el indice (id)
+		this.router.navigate( ['/heroe', idx] )
+
+	}
 
 }
